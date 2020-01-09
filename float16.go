c8df72cfeb6eac70c286bd668596280d9134bae1
x448 <57072051+x448@users.noreply.github.com>
2020-01-09 17:50:53 -0600

    Fix comment that had cut/paste error

--- float16.go
+++ float16.go
@@ -20,13 +20,17 @@ type Precision int
 
 const (
 
-	// PrecisionExact is for subnormals that don't drop bits during conversion but not all of these can round-trip.
+	// PrecisionExact is for non-subnormals that don't drop bits during conversion.
+	// All of these can round-trip.  Should always convert to float16.
 	PrecisionExact Precision = iota
 
-	// PrecisionUnknown is for Subnormals that don't drop bits during conversion but not all of these can round-trip.
+	// PrecisionUnknown is for subnormals that don't drop bits during conversion but
+	// not all of these can round-trip so precision is unknown without more effort.
+	// Only 2046 of these can round-trip and the rest cannot round-trip.
 	PrecisionUnknown
 
-	// PrecisionInexact is for dropped significand bits and cannot round-trip. Some of these are subnormals.
+	// PrecisionInexact is for dropped significand bits and cannot round-trip.
+	// Some of these are subnormals. Cannot round-trip float32->float16->float32.
 	PrecisionInexact
 
 	// PrecisionUnderflow is for Underflows. Cannot round-trip float32->float16->float32.
